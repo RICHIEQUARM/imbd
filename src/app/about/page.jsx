da6ddf980d396acbb3308ca66f3b309be77a83db
richie <conduahrichard5@gmail.com>
2024-06-07 19:50:27 -0700

    add the about page

---
+++ src/app/about/page.jsx
@@ -0,0 +1,45 @@
+export default function page() {
+  return (
+    <div className="max-w-6xl mx-auto  p-3 space-y-4">
+      <h1 className="text-2xl text-medium text-amber-500">About</h1>
+      <p>
+        Welcome to the Ultimate Movie Page, your one-stop destination for all
+        things cinema. Whether you are a casual viewer or a die-hard film buff,
+        our site offers an extensive database of movies from every genre and
+        era. Discover detailed synopses, cast and crew information, and
+        insightful reviews that will guide you in selecting your next watch. Our
+        intuitive search and filter features make it easy to find movies that
+        match your tastes, from blockbuster hits to hidden gems
+      </p>
+
+      <p>
+        Our website is designed to provide you with comprehensive database of
+        movies from all around the world, along with the latest news, reviews,
+        and trailers. Our movie database is constantly updated with new
+        releases, ensuring that you have access to the latest and greatest in
+        the world of Cinemas. You can search for movies by title, director,
+        actor genre, or release date, making it easy to find the perfect movie
+        for any occasion.
+      </p>
+
+      <p>
+        In addition to comprehensive movie details, the Ultimate Movie Page
+        provides up-to-date news and trailers, keeping you informed about the
+        latest releases and industry buzz. Our expert articles and editorials
+        offer deep dives into film history, trends, and notable achievements in
+        filmmaking. Join our community of movie enthusiasts to discuss your
+        favorite films, share recommendations, and engage in lively debates
+        about the latest cinematic trends and controversies.
+      </p>
+
+      <p>
+        Enhance your movie-watching experience with personalized recommendations
+        and curated watchlists. By creating an account, you can rate movies,
+        track what you have watched, and set up notifications for upcoming
+        releases and special screenings. With the Ultimate Movie Page, you are
+        not just finding movies you are exploring a world of cinematic wonder
+        and connecting with fellow movie lovers who share your passion.
+      </p>
+    </div>
+  );
+}
